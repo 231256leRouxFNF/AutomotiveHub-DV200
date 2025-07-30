f9ea2220e11e1ec2aad876f2b61f87ec1f045ca6
FrancoisleRoux <159991396+231256leRouxFNF@users.noreply.github.com>
2025-07-30 17:22:47 +0200

    did it in the wrong order

--- client/src/components/LoginLayout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoginLayout = ({ children }) => (
-  <div className="login-layout" style={{ display: 'flex', minHeight: '100vh', background: '#fafbfc' }}>
-    {children}
-  </div>
-);
-
-export default LoginLayout;
